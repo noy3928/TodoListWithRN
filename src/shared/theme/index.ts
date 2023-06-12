2c99728172bb85e174e2d1ebbc827685f095a63a
yechan <noy3928@naver.com>
2023-06-12 23:38:09 +0900

    feat : setting theme color file

---
+++ src/shared/theme/index.ts
@@ -0,0 +1,31 @@
+import { Appearance } from "react-native"
+
+import { THEME } from "../constants"
+
+export const themeJson: ITheme = require("./theme.json")
+
+interface ITheme {
+  [theme: string]: ThemeInterface
+}
+
+export interface ThemeInterface {
+  theme: string
+  background: string
+  surface: string
+  primary: string
+}
+
+class ThemeStore {
+  defaultTheme = THEME.LIGHT
+
+  constructor() {
+    const deviceTheme = Appearance.getColorScheme()
+    if (deviceTheme) {
+      this.defaultTheme = deviceTheme
+    }
+  }
+}
+
+export const themeStore = new ThemeStore()
+const theme = themeJson[themeStore.defaultTheme]
+export default theme
