a694b4118fb4a3d22b09b6297d89b3a380d10134
yechan <noy3928@naver.com>
2023-06-14 17:42:15 +0900

    feat : make modal state

--- src/store/slices/index.ts
+++ src/store/slices/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit"
 import todoReducer from "./todos"
+import modalReducer from "./modal"
 
 const rootReducer = combineReducers({
   todos: todoReducer,
+  modal: modalReducer,
 })
 
 export default rootReducer
